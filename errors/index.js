4e0e154a31a72238c8273e5664e9154647f9b8ff
Robin Pillar <rpillar72@gmail.com>
2021-01-05 14:22:05 +0000

    removed console.log

--- errors/index.js
+++ errors/index.js
@@ -17,7 +17,6 @@ exports.handle422s = (err, req, res, next) => {
 };
 
 exports.handle500s = (err, req, res, next) => {
-  console.log(err);
   res.status(500).send({ msg: 'Internal Server Error' });
 };
 

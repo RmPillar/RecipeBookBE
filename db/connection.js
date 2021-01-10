abb572b7b936c14f15c38e64acced579557ea633
Robin Pillar <rpillar72@gmail.com>
2021-01-10 16:44:43 +0000

    Attempt to fix connection issues

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,13 @@ const connection = require('knex');
 
 const dbConfig =
   ENV === 'production'
-    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    ? {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        extra: {
+          ssl: true,
+        },
+      }
     : require('../knexfile');
 
 module.exports = connection(dbConfig);
